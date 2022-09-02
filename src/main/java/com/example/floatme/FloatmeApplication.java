d8a8a92700ca99ef913b5429d2b54d9a580feb1b
Trevor Esparza <trevoresparza30@gmail.com>
2022-09-02 14:24:42 -0500

    first commit

---
+++ src/main/java/com/example/floatme/FloatmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.floatme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloatmeApplication.class, args);
+    }
+
+}
