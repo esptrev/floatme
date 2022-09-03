fe2c5c39b241ff51fb3c476697cdf68687152029
Trevor Esparza <trevoresparza30@gmail.com>
2022-09-02 20:50:11 -0500

    deleted user/update person

--- src/main/java/com/example/floatme/data/Person.java
+++ src/main/java/com/example/floatme/data/Person.java
@@ -1,6 +1,15 @@
 package com.example.floatme.data;
+import lombok.*;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Person {
-    public Person() {
-    }
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
 }
