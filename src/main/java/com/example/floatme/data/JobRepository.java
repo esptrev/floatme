0075bcb542e3fb9ff634b85fbb90436003b18ceb
Trevor Esparza <trevoresparza30@gmail.com>
2022-09-03 09:34:56 -0500

    repositories

---
+++ src/main/java/com/example/floatme/data/JobRepository.java
@@ -0,0 +1,8 @@
+package com.example.floatme.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+
+
+}
---
+++ src/main/java/com/example/floatme/data/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.example.floatme.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person,Long> {
+
+}
